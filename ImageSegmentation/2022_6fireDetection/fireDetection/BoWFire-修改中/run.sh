dec3d12e277f9e3f22707334e5ccfe9dfcb71d2e
RainyMacondo <yaofanghao7@163.com>
2022-06-26 23:44:33 +0800

    2022.5.18

--- ImageSegmentation/2022_6fireDetection/fireDetection/BoWFire-修改中/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# train the model
-echo "Training the model..."
-python train.py
-echo "Training completed!"
-# test the model 
-echo "Testing the model"
-python test.py  --color_space True --color_component True  --texture True
-echo "Testing completed!"
