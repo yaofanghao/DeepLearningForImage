46d8610c4af79544913c01999e6658b6a4353545
RainyMacondo <yaofanghao7@163.com>
2022-07-14 21:17:15 +0800

    2022.7.14

---
+++ PyQt_project/EGC_yolo_predict/yolo_predict11-修改中/predict11.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from PIL import Image
+from yolo_predict3 import YOLO
+
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+if __name__ == "__main__":
+    yolo = YOLO()
+    mode = "predict"
+    crop = False
+    count = False
+
+    if mode == "predict":
+        '''
+        1、如果想要进行检测完的图片的保存，利用r_image.save("img.jpg")即可保存，直接在predict.py里进行修改即可。 
+        2、如果想要获得预测框的坐标，可以进入yolo.detect_image函数，在绘图部分读取top，left，bottom，right这四个值。
+        3、如果想要利用预测框截取下目标，可以进入yolo.detect_image函数，在绘图部分利用获取到的top，left，bottom，right这四个值
+        在原图上利用矩阵的方式进行截取。
+        4、如果想要在预测图上写额外的字，比如检测到的特定目标的数量，可以进入yolo.detect_image函数，在绘图部分对predicted_class进行判断，
+        比如判断if predicted_class == 'car': 即可判断当前目标是否为车，然后记录数量即可。利用draw.text即可写字。
+        '''
+        while True:
+            img = input('Input image filename:')
+            try:
+                image = Image.open(img)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                r_image, out_scores, out_classes = yolo.detect_image(image, crop=crop, count=count)
+                r_image.show()
+
+    else:
+        raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps' or 'dir_predict'.")
