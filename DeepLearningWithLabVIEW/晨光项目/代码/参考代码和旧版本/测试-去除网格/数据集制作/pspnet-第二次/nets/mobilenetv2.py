74225e9c6c7f7e1209a82a82dd28de343829468a
yaofanghao <yaofanghao7@163.com>
2023-02-21 17:15:45 +0800

    2023.2.21- old version

---
+++ DeepLearningWithLabVIEW/晨光项目/代码/参考代码和旧版本/测试-去除网格/数据集制作/pspnet-第二次/nets/mobilenetv2.py
@@ -0,0 +1,142 @@
+from tensorflow.keras.activations import relu
+from tensorflow.keras.layers import (Activation, Add, BatchNormalization,
+                                     Conv2D, DepthwiseConv2D, Input)
+
+
+def _make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+def relu6(x):
+    return relu(x, max_value=6)
+
+def _inverted_res_block(inputs, expansion, stride, alpha, in_filters, filters, block_id, skip_connection, rate=1):
+    pointwise_conv_filters = int(filters * alpha)
+    pointwise_filters = _make_divisible(pointwise_conv_filters, 8)
+    x = inputs
+    prefix = 'expanded_conv_{}_'.format(block_id)
+
+    #----------------------------------------------------#
+    #   利用1x1卷积根据输入进来的通道数进行通道数上升
+    #----------------------------------------------------#
+    if block_id:
+        x = Conv2D(expansion * in_filters, kernel_size=1, padding='same',
+                   use_bias=False, activation=None,
+                   name=prefix + 'expand')(x)
+        x = BatchNormalization(epsilon=1e-3, momentum=0.999,
+                               name=prefix + 'expand_BN')(x)
+        x = Activation(relu6, name=prefix + 'expand_relu')(x)
+    else:
+        prefix = 'expanded_conv_'
+
+    #----------------------------------------------------#
+    #   利用深度可分离卷积进行特征提取
+    #----------------------------------------------------#
+    x = DepthwiseConv2D(kernel_size=3, strides=stride, activation=None,
+                        use_bias=False, padding='same', dilation_rate=(rate, rate),
+                        name=prefix + 'depthwise')(x)
+    x = BatchNormalization(epsilon=1e-3, momentum=0.999,
+                           name=prefix + 'depthwise_BN')(x)
+
+    x = Activation(relu6, name=prefix + 'depthwise_relu')(x)
+
+    #----------------------------------------------------#
+    #   利用1x1的卷积进行通道数的下降
+    #----------------------------------------------------#
+    x = Conv2D(pointwise_filters,
+               kernel_size=1, padding='same', use_bias=False, activation=None,
+               name=prefix + 'project')(x)
+    x = BatchNormalization(epsilon=1e-3, momentum=0.999,
+                           name=prefix + 'project_BN')(x)
+
+    #----------------------------------------------------#
+    #   添加残差边
+    #----------------------------------------------------#
+    if skip_connection:
+        return Add(name=prefix + 'add')([inputs, x])
+    return x
+
+def get_mobilenet_encoder(inputs_size, downsample_factor=8):
+    if downsample_factor == 16:
+        block4_dilation = 1
+        block5_dilation = 2
+        block4_stride = 2
+    elif downsample_factor == 8:
+        block4_dilation = 2
+        block5_dilation = 4
+        block4_stride = 1
+    else:
+        raise ValueError('Unsupported factor - `{}`, Use 8 or 16.'.format(downsample_factor))
+    
+    # 473,473,3
+    inputs = Input(shape=inputs_size)
+
+    alpha=1.0
+    first_block_filters = _make_divisible(32 * alpha, 8)
+
+    # 473,473,3 -> 237,237,32
+    x = Conv2D(first_block_filters,
+                kernel_size=3,
+                strides=(2, 2), padding='same',
+                use_bias=False, name='Conv')(inputs)
+    x = BatchNormalization(epsilon=1e-3, momentum=0.999, name='Conv_BN')(x)
+    x = Activation(relu6, name='Conv_Relu6')(x)
+
+    # 237,237,32 -> 237,237,16
+    x = _inverted_res_block(x, in_filters=32, filters=16, alpha=alpha, stride=1,
+                            expansion=1, block_id=0, skip_connection=False)
+
+    #---------------------------------------------------------------#
+    # 237,237,16 -> 119,119,24
+    x = _inverted_res_block(x, in_filters=16, filters=24, alpha=alpha, stride=2,
+                            expansion=6, block_id=1, skip_connection=False)
+    x = _inverted_res_block(x, in_filters=24, filters=24, alpha=alpha, stride=1,
+                            expansion=6, block_id=2, skip_connection=True)
+                            
+    #---------------------------------------------------------------#
+    # 119,119,24 -> 60,60.32
+    x = _inverted_res_block(x, in_filters=24, filters=32, alpha=alpha, stride=2,
+                            expansion=6, block_id=3, skip_connection=False)
+    x = _inverted_res_block(x, in_filters=32, filters=32, alpha=alpha, stride=1,
+                            expansion=6, block_id=4, skip_connection=True)
+    x = _inverted_res_block(x, in_filters=32, filters=32, alpha=alpha, stride=1,
+                            expansion=6, block_id=5, skip_connection=True)
+
+    #---------------------------------------------------------------#
+    # 60,60,32 -> 30,30.64
+    x = _inverted_res_block(x, in_filters=32, filters=64, alpha=alpha, stride=block4_stride,
+                            expansion=6, block_id=6, skip_connection=False)
+    x = _inverted_res_block(x, in_filters=64, filters=64, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=7, skip_connection=True)
+    x = _inverted_res_block(x, in_filters=64, filters=64, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=8, skip_connection=True)
+    x = _inverted_res_block(x, in_filters=64, filters=64, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=9, skip_connection=True)
+
+    # 30,30.64 -> 30,30.96
+    x = _inverted_res_block(x, in_filters=64, filters=96, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=10, skip_connection=False)
+    x = _inverted_res_block(x, in_filters=96, filters=96, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=11, skip_connection=True)
+    x = _inverted_res_block(x, in_filters=96, filters=96, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=12, skip_connection=True)
+    # 辅助分支训练
+    f4 = x
+
+    #---------------------------------------------------------------#
+    # 30,30.96 -> 30,30,160 -> 30,30,320
+    x = _inverted_res_block(x, in_filters=96, filters=160, alpha=alpha, stride=1, rate=block4_dilation,
+                            expansion=6, block_id=13, skip_connection=False)
+    x = _inverted_res_block(x, in_filters=160, filters=160, alpha=alpha, stride=1, rate=block5_dilation,
+                            expansion=6, block_id=14, skip_connection=True)
+    x = _inverted_res_block(x, in_filters=160, filters=160, alpha=alpha, stride=1, rate=block5_dilation,
+                            expansion=6, block_id=15, skip_connection=True)
+
+    x = _inverted_res_block(x, in_filters=160, filters=320, alpha=alpha, stride=1, rate=block5_dilation,
+                            expansion=6, block_id=16, skip_connection=False)
+    f5 = x
+    return inputs, f4, f5
---
+++ DeepLearningWithLabVIEW/晨光项目/代码/参考代码和旧版本/测试-去除网格/数据集制作/pspnet-第二次/predict.py
@@ -0,0 +1,67 @@
+import time
+import cv2
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import os
+from tqdm import tqdm
+
+from pspnet import Pspnet
+
+gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+    
+if __name__ == "__main__":
+    #-------------------------------------------------------------------------#
+    #   如果想要修改对应种类的颜色，到__init__函数里修改self.colors即可
+    #-------------------------------------------------------------------------#
+    pspnet = Pspnet()
+    mode = "dir_predict"
+    img_name = "test4_out_8bit.bmp"
+    count           = False
+    name_classes    = ["background","grid"]
+    dir_origin_path = "img/"
+    dir_save_path   = "img_out/"
+    if not os.path.exists(dir_save_path):
+        os.makedirs(dir_save_path)
+
+    if mode == "predict":
+        '''
+        predict.py有几个注意点
+        1、该代码无法直接进行批量预测，如果想要批量预测，可以利用os.listdir()遍历文件夹，利用Image.open打开图片文件进行预测。
+        具体流程可以参考get_miou_prediction.py，在get_miou_prediction.py即实现了遍历。
+        2、如果想要保存，利用r_image.save("img.jpg")即可保存。
+        3、如果想要原图和分割图不混合，可以把blend参数设置成False。
+        4、如果想根据mask获取对应的区域，可以参考detect_image函数中，利用预测结果绘图的部分，判断每一个像素点的种类，然后根据种类获取对应的部分。
+        seg_img = np.zeros((np.shape(pr)[0],np.shape(pr)[1],3))
+        for c in range(self.num_classes):
+            seg_img[:, :, 0] += ((pr == c)*( self.colors[c][0] )).astype('uint8')
+            seg_img[:, :, 1] += ((pr == c)*( self.colors[c][1] )).astype('uint8')
+            seg_img[:, :, 2] += ((pr == c)*( self.colors[c][2] )).astype('uint8')
+        '''
+        while True:
+            try:
+                image = Image.open(img_name)
+            except:
+                print('Open Error! Try again!')
+                continue
+            else:
+                r_image = pspnet.detect_image(image, count=count, name_classes=name_classes)
+                # r_image.show()
+                r_image.save(os.path.join(dir_save_path, img_name))
+                print("predict done")
+
+    elif mode == "dir_predict":
+        img_names = os.listdir(dir_origin_path)
+        for img_name in tqdm(img_names):
+            if img_name.lower().endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                image_path  = os.path.join(dir_origin_path, img_name)
+                image       = Image.open(image_path)
+                r_image     = pspnet.detect_image(image)
+                if not os.path.exists(dir_save_path):
+                    os.makedirs(dir_save_path)
+                r_image.save(os.path.join(dir_save_path, img_name))
+                
+    else:
+        raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps' or 'dir_predict'.")
