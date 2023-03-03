3329ab91596a66b42f38fcd710812b8b693b2461
yaofanghao <yaofanghao7@163.com>
2023-03-03 17:04:12 +0800

    2023.3.3 update hrnet1.py

---
+++ DeepLearningWithLabVIEW/内窥缺陷检测项目/hrnet/hrnet1.py
@@ -0,0 +1,232 @@
+import colorsys
+import copy
+import time
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from PIL import Image,ImageFont,ImageDraw
+
+from nets.hrnet import HRnet
+from utils.utils import cvtColor, preprocess_input, resize_image, show_config
+
+class HRnet_Segmentation(object):
+    _defaults = {
+        "model_path"        : 'logs/best_epoch_weights.h5',
+        "num_classes"       : 8,    #   所需要区分的类的个数+1
+        #----------------------------------------#
+        #   所使用的的主干网络：
+        #   hrnetv2_w18
+        #   hrnetv2_w32
+        #   hrnetv2_w48  
+        #----------------------------------------#
+        "backbone"          : "hrnetv2_w32",
+        #----------------------------------------#
+        #   输入图片的大小
+        #----------------------------------------#
+        "input_shape"       : [480, 480],
+    }
+
+    #---------------------------------------------------#
+    #   初始化Deeplab
+    #---------------------------------------------------#
+    def __init__(self, **kwargs):
+        self.__dict__.update(self._defaults)
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+        #---------------------------------------------------#
+        #   画框设置不同的颜色
+        #---------------------------------------------------#
+        if self.num_classes <= 21:
+            self.colors = [ (0, 0, 0), (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128), (0, 128, 128), 
+                            (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0), (192, 128, 0), (64, 0, 128), (192, 0, 128), 
+                            (64, 128, 128), (192, 128, 128), (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128), 
+                            (128, 64, 12)]
+        else:
+            hsv_tuples = [(x / self.num_classes, 1., 1.) for x in range(self.num_classes)]
+            self.colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+            self.colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), self.colors))
+        #---------------------------------------------------#
+        #   获得模型
+        #---------------------------------------------------#
+        self.generate()
+        
+        show_config(**self._defaults)
+
+    #---------------------------------------------------#
+    #   获得所有的分类
+    #---------------------------------------------------#
+    def generate(self):
+        #-------------------------------#
+        #   载入模型与权值
+        #-------------------------------#
+        self.model = HRnet([self.input_shape[0], self.input_shape[1], 3], self.num_classes, backbone = self.backbone)
+
+        self.model.load_weights(self.model_path)
+        print('{} model loaded.'.format(self.model_path))
+        
+        
+    @tf.function
+    def get_pred(self, photo):
+        preds = self.model(photo, training=False)
+        return preds
+    
+    #---------------------------------------------------#
+    #   检测图片
+    #---------------------------------------------------#
+    def detect_image(self, image, name_classes=None):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------#
+        #   对输入图像进行一个备份，后面用于绘图
+        #---------------------------------------------------#
+        old_img     = copy.deepcopy(image)
+        orininal_h  = np.array(image).shape[0]
+        orininal_w  = np.array(image).shape[1]
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #---------------------------------------------------------#
+        image_data, nw, nh  = resize_image(image, (self.input_shape[1], self.input_shape[0]))
+        #---------------------------------------------------------#
+        #   归一化+添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = np.expand_dims(preprocess_input(np.array(image_data, np.float32)), 0)
+
+        #---------------------------------------------------#
+        #   图片传入网络进行预测
+        #---------------------------------------------------#
+        pr = self.get_pred(image_data)[0].numpy()
+        #---------------------------------------------------#
+        #   将灰条部分截取掉
+        #---------------------------------------------------#
+        pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+        #---------------------------------------------------#
+        #   进行图片的resize
+        #---------------------------------------------------#
+        pr = cv2.resize(pr, (orininal_w, orininal_h), interpolation = cv2.INTER_LINEAR)
+        #---------------------------------------------------#
+        #   取出每一个像素点的种类
+        #---------------------------------------------------#
+        pr = pr.argmax(axis=-1)
+
+        # seg_img = np.zeros((np.shape(pr)[0], np.shape(pr)[1], 3))
+        # for c in range(self.num_classes):
+        #     seg_img[:, :, 0] += ((pr[:, :] == c ) * self.colors[c][0]).astype('uint8')
+        #     seg_img[:, :, 1] += ((pr[:, :] == c ) * self.colors[c][1]).astype('uint8')
+        #     seg_img[:, :, 2] += ((pr[:, :] == c ) * self.colors[c][2]).astype('uint8')
+        seg_img = np.reshape(np.array(self.colors, np.uint8)[np.reshape(pr, [-1])], [orininal_h, orininal_w, -1])
+        image   = Image.fromarray(np.uint8(seg_img))
+        image   = Image.blend(old_img, image, 0.7)
+
+        # 修改显示字体
+        font = ImageFont.truetype(font='model_data/simhei.ttf',
+                                  size=np.floor(3e-2 * image.size[1] + 20).astype('int32')) # 修改字体大小
+        draw = ImageDraw.Draw(image)
+        classes_nums = np.zeros([self.num_classes])
+        total_points_num = orininal_h * orininal_w
+        # print('-' * 63)
+        # print("|%25s | %15s | %15s|" % ("Key", "Value", "Ratio"))
+        # print('-' * 63)
+        output_class_name =   np.array([0])  # 存放预测类别结果的数组
+        for i in range(self.num_classes):
+            num     = np.sum(pr == i)
+            ratio   = num / total_points_num * 100
+            if  (num > 0) & (name_classes!=None):
+                # print("|%25s | %15s | %14.2f%%|"%(str(name_classes[i]), str(num), ratio))
+                # print('-' * 63)
+                draw.text((50*i,50*i),str(name_classes[i]),fill='red',font=font)
+                output_class_name = np.append(output_class_name, i)
+            classes_nums[i] = num
+        # print("classes_nums:", classes_nums)
+        return image, output_class_name
+
+    def get_FPS(self, image, test_interval):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #---------------------------------------------------------#
+        image_data, nw, nh  = resize_image(image, (self.input_shape[1], self.input_shape[0]))
+        #---------------------------------------------------------#
+        #   归一化+添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = np.expand_dims(preprocess_input(np.array(image_data, np.float32)), 0)
+
+        #---------------------------------------------------#
+        #   图片传入网络进行预测
+        #---------------------------------------------------#
+        pr = self.get_pred(image_data)[0].numpy()
+        #--------------------------------------#
+        #   将灰条部分截取掉
+        #--------------------------------------#
+        pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+        #---------------------------------------------------#
+        #   取出每一个像素点的种类
+        #---------------------------------------------------#
+        pr = pr.argmax(axis=-1).reshape([self.input_shape[0],self.input_shape[1]])
+                
+        t1 = time.time()
+        for _ in range(test_interval):
+            #---------------------------------------------------#
+            #   图片传入网络进行预测
+            #---------------------------------------------------#
+            pr = self.get_pred(image_data)[0].numpy()
+            #--------------------------------------#
+            #   将灰条部分截取掉
+            #--------------------------------------#
+            pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                    int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+            #---------------------------------------------------#
+            #   取出每一个像素点的种类
+            #---------------------------------------------------#
+            pr = pr.argmax(axis=-1).reshape([self.input_shape[0],self.input_shape[1]])
+
+        t2 = time.time()
+        tact_time = (t2 - t1) / test_interval
+        return tact_time
+        
+    def get_miou_png(self, image):
+        #---------------------------------------------------------#
+        #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
+        #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+        #---------------------------------------------------------#
+        image       = cvtColor(image)
+        orininal_h  = np.array(image).shape[0]
+        orininal_w  = np.array(image).shape[1]
+        #---------------------------------------------------------#
+        #   给图像增加灰条，实现不失真的resize
+        #---------------------------------------------------------#
+        image_data, nw, nh  = resize_image(image, (self.input_shape[1], self.input_shape[0]))
+        #---------------------------------------------------------#
+        #   归一化+添加上batch_size维度
+        #---------------------------------------------------------#
+        image_data  = np.expand_dims(preprocess_input(np.array(image_data, np.float32)), 0)
+
+        #--------------------------------------#
+        #   图片传入网络进行预测
+        #--------------------------------------#
+        pr = self.get_pred(image_data)[0].numpy()
+        #--------------------------------------#
+        #   将灰条部分截取掉
+        #--------------------------------------#
+        pr = pr[int((self.input_shape[0] - nh) // 2) : int((self.input_shape[0] - nh) // 2 + nh), \
+                int((self.input_shape[1] - nw) // 2) : int((self.input_shape[1] - nw) // 2 + nw)]
+        #--------------------------------------#
+        #   进行图片的resize
+        #--------------------------------------#
+        pr = cv2.resize(pr, (orininal_w, orininal_h), interpolation = cv2.INTER_LINEAR)
+        #---------------------------------------------------#
+        #   取出每一个像素点的种类
+        #---------------------------------------------------#
+        pr = pr.argmax(axis=-1)
+
+        image = Image.fromarray(np.uint8(pr))
+        return image
